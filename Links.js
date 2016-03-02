4c14deb9e82e4231cfe16267fd25e68d61362554
lsmith2 <sliam0208@gmail.com>
2016-03-01 21:04:43 -0500

    Update Links.js

--- Links.js
+++ Links.js
@@ -1,19 +1,19 @@
 function CoCaRP() {
-    window.location.href = "/CodeofConductAndRefundPolicy.html";
+    window.location.href = "c4a-conclave/CodeofConductAndRefundPolicy.html";
 }
 
 function OtherInfoLink() {
-    window.location.href = "/OtherInfo.html";
+    window.location.href = "c4a-conclave/OtherInfo.html";
 }
 
 function DestinationLink() {
-    window.location.href = "/destination.html";
+    window.location.href = "c4a-conclave/destination.html";
 }
 
 function TrainingLink() {
-    window.location.href = "/training.html";
+    window.location.href = "c4a-conclave/training.html";
 }
 
 function RegistrationPage() {
-    window.location.href = "/Registration.html";
+    window.location.href = "c4a-conclave/Registration.html";
 }
