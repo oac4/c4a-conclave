5bfbd0473ecd08a2157a0358a8208b23c06f0066
lsmith2 <sliam0208@gmail.com>
2016-03-01 21:08:32 -0500

    Update Links.js

--- Links.js
+++ Links.js
@@ -1,19 +1,19 @@
 function CoCaRP() {
-    window.location.href = "/CodeofConductAndRefundPolicy.html";
+    window.location.href = "CodeofConductAndRefundPolicy.html";
 }
 
 function OtherInfoLink() {
-    window.location.href = "/OtherInfo.html";
+    window.location.href = "OtherInfo.html";
 }
 
 function DestinationLink() {
-    window.location.href = "/destination.html";
+    window.location.href = "destination.html";
 }
 
 function TrainingLink() {
-    window.location.href = "/training.html";
+    window.location.href = "training.html";
 }
 
 function RegistrationPage() {
-    window.location.href = "/Registration.html";
+    window.location.href = "Registration.html";
 }
